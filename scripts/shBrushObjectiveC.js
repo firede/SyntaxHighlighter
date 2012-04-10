5f5b8df75f4b9f1b52acf5a205010fdaa1bd70f8
Zhao Lei <aicoylei@gmail.com>
2012-04-10 16:26:46 +0800

    modify shBrushObjectiveC.js

--- scripts/shBrushObjectiveC.js
+++ scripts/shBrushObjectiveC.js
@@ -1,5 +1,9 @@
 /**
  * SyntaxHighlighter - Objective-C Brush
+ * @version 1.0.1 (April 10 2012)
+ * @author Firede
+ * 
+ * Update From:
  * http://codepirate.seaandco.com/
  *
  * @version
@@ -21,37 +25,55 @@
  *  http://scottdensmore.typepad.com/blog/2008/12/objective-c-cocoa-syntax-highlighter.html
  */
 
-SyntaxHighlighter.brushes.ObjC = function()
+;(function()
 {
-    var datatypes = 'char bool BOOL double float int long short id void';
-    
-    var keywords = 'IBAction IBOutlet SEL YES NO readwrite readonly nonatomic nil NULL ';
-    keywords += 'super self copy ';
-    keywords += 'break case catch class const copy __finally __exception __try ';
-    keywords += 'const_cast continue private public protected __declspec ';
-    keywords += 'default delete deprecated dllexport dllimport do dynamic_cast ';
-    keywords += 'else enum explicit extern if for friend goto inline ';
-    keywords += 'mutable naked namespace new noinline noreturn nothrow ';
-    keywords += 'register reinterpret_cast return selectany ';
-    keywords += 'sizeof static static_cast struct switch template this ';
-    keywords += 'thread throw true false try typedef typeid typename union ';
-    keywords += 'using uuid virtual volatile whcar_t while';
-    // keywords += '@property @selector @interface @end @implementation @synthesize ';
-    
-        
-    this.regexList = [
-        { regex: SyntaxHighlighter.regexLib.singleLineCComments,        css: 'comments' },      // one line comments
-        { regex: SyntaxHighlighter.regexLib.multiLineCComments,     css: 'comments' },      // multiline comments
-        { regex: SyntaxHighlighter.regexLib.doubleQuotedString,     css: 'string' },            // double quoted strings
-        { regex: SyntaxHighlighter.regexLib.singleQuotedString,     css: 'string' },            // single quoted strings
-        { regex: new RegExp('^ *#.*', 'gm'),                        css: 'preprocessor' },      // preprocessor
-        { regex: new RegExp(this.getKeywords(datatypes), 'gm'),     css: 'datatypes' },     // datatypes
-        { regex: new RegExp(this.getKeywords(keywords), 'gm'),      css: 'keyword' },           // keyword
-        { regex: new RegExp('\\bNS\\w+\\b', 'g'),                   css: 'keyword' },           // keyword
-        { regex: new RegExp('@\\w+\\b', 'g'),                       css: 'keyword' },           // keyword
-        ];
-    
-}
+    // CommonJS
+    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-SyntaxHighlighter.brushes.ObjC.prototype = new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.ObjC.aliases = ['objc', 'obj-c'];
+    function Brush()
+    {
+        var datatypes = 'char bool BOOL double float int long short id void';
+
+        var keywords =  'abstract as base bool break byte case catch char checked class const ' +
+                        'continue decimal default delegate do double else enum event explicit ' +
+                        'extern false finally fixed float for foreach get goto if implicit in int ' +
+                        'interface internal is lock long namespace new null object operator out ' +
+                        'override params private protected public readonly ref return sbyte sealed set ' +
+                        'short sizeof stackalloc static string struct switch this throw true try ' +
+                        'typeof uint ulong unchecked unsafe ushort using virtual void while var';
+
+        var keywords =  'IBAction IBOutlet SEL YES NO readwrite readonly nonatomic nil NULL ' +
+                        'super self copy ' +
+                        'break case catch class const copy __finally __exception __try ' +
+                        'const_cast continue private public protected __declspec ' +
+                        'default delete deprecated dllexport dllimport do dynamic_cast ' +
+                        'else enum explicit extern if for friend goto inline ' +
+                        'mutable naked namespace new noinline noreturn nothrow ' +
+                        'register reinterpret_cast return selectany ' +
+                        'sizeof static static_cast struct switch template this ' +
+                        'thread throw true false try typedef typeid typename union ' +
+                        'using uuid virtual volatile whcar_t while';
+
+        // keywords += '@property @selector @interface @end @implementation @synthesize ';
+
+        this.regexList = [
+            { regex: SyntaxHighlighter.regexLib.singleLineCComments,        css: 'comments' },      // one line comments
+            { regex: SyntaxHighlighter.regexLib.multiLineCComments,     css: 'comments' },      // multiline comments
+            { regex: SyntaxHighlighter.regexLib.doubleQuotedString,     css: 'string' },            // double quoted strings
+            { regex: SyntaxHighlighter.regexLib.singleQuotedString,     css: 'string' },            // single quoted strings
+            { regex: new RegExp('^ *#.*', 'gm'),                        css: 'preprocessor' },      // preprocessor
+            { regex: new RegExp(this.getKeywords(datatypes), 'gm'),     css: 'datatypes' },     // datatypes
+            { regex: new RegExp(this.getKeywords(keywords), 'gm'),      css: 'keyword' },           // keyword
+            { regex: new RegExp('\\bNS\\w+\\b', 'g'),                   css: 'keyword' },           // keyword
+            { regex: new RegExp('@\\w+\\b', 'g'),                       css: 'keyword' },           // keyword
+        ];
+    };
+
+    Brush.prototype = new SyntaxHighlighter.Highlighter();
+    Brush.aliases   = ['objc', 'obj-c'];
+
+    SyntaxHighlighter.brushes.ObjC = Brush;
+
+    // CommonJS
+    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
